3d489ccd2195d8c66ed224f1f96ea6a7db619749
edvinand <edvinholmseth@gmail.com>
2021-12-07 15:52:45 +0100

    first commit

---
+++ temp_files/snapshot3/remote_service/remote.h
@@ -0,0 +1,36 @@
+
+#include <zephyr.h>
+#include <logging/log.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/hci.h>
+
+/** @brief UUID of the Remote Service. **/
+#define BT_UUID_REMOTE_SERV_VAL \
+	BT_UUID_128_ENCODE(0xe9ea0001, 0xe19b, 0x482d, 0x9293, 0xc7907585fc48)
+
+/** @brief UUID of the Button Characteristic. **/
+#define BT_UUID_REMOTE_BUTTON_CHRC_VAL \
+	BT_UUID_128_ENCODE(0xe9ea0002, 0xe19b, 0x482d, 0x9293, 0xc7907585fc48)
+
+/** @brief UUID of the Message Characteristic. **/
+#define BT_UUID_REMOTE_MESSAGE_CHRC_VAL \
+	BT_UUID_128_ENCODE(0xe9ea0003, 0xe19b, 0x482d, 0x9293, 0xc7907585fc48)
+
+#define BT_UUID_REMOTE_SERVICE  BT_UUID_DECLARE_128(BT_UUID_REMOTE_SERV_VAL)
+#define BT_UUID_REMOTE_BUTTON_CHRC 	BT_UUID_DECLARE_128(BT_UUID_REMOTE_BUTTON_CHRC_VAL)
+
+enum bt_button_notifications_enabled {
+	BT_BUTTON_NOTIFICATIONS_ENABLED,
+	BT_BUTTON_NOTIFICATIONS_DISABLED,
+};
+
+struct bt_remote_service_cb {
+	void (*notif_changed)(enum bt_button_notifications_enabled status);
+};
+
+int send_button_notification(struct bt_conn *conn, uint8_t value);
+void set_button_value(uint8_t btn_value);
+int bluetooth_init(struct bt_conn_cb *bt_cb, struct bt_remote_service_cb *remote_cb);
